06b7131a2ea78d97189265ae8b42910b6042a2bc
fanny <service@tradplus.com>
2021-04-21 15:29:54 +0800

    V5.6.3 更新穿山甲国内

--- TradPlusAndroidDemo/app/src/main/java/com/tradplus/demo/MainActivity.java
+++ TradPlusAndroidDemo/app/src/main/java/com/tradplus/demo/MainActivity.java
@@ -11,14 +11,17 @@ import android.widget.CompoundButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.tradplus.ads.mobileads.TradPlus;
+import com.tradplus.ads.mobileads.gdpr.ATGDPRAuthCallback;
 import com.tradplus.ads.mobileads.gdpr.Const;
 import com.tradplus.ads.mobileads.util.TestDeviceUtil;
-import com.tradplus.ads.open.TradPlusSdk;
 import com.tradplus.demo.banners.BannerActivity;
 import com.tradplus.demo.interstititals.InterstitialActivity;
 import com.tradplus.demo.nativeads.DrawNativeExpressVideoActivity;
 import com.tradplus.demo.nativeads.NativeActivity;
 import com.tradplus.demo.nativeads.NativeBannerViewActivity;
+import com.tradplus.demo.nativeads.NativeCacheActivity;
 import com.tradplus.demo.nativeads.NativeSlotActivity;
 import com.tradplus.demo.offerwall.OfferWallActivity;
 import com.tradplus.demo.rewarded.RewardedVideoActivity;
@@ -64,12 +67,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         gdpr_neu.setOnClickListener(this);
         gdpr_yeu = (Button) findViewById(R.id.btn_gdpr_yeu);
         gdpr_yeu.setOnClickListener(this);
+        btn_box = (CheckBox) findViewById(R.id.btn_box);
         gpdrChild = (CheckBox) findViewById(R.id.is_gdpr_child);
 
         custom_gdpr_sdk_init = (Button) findViewById(R.id.btn_custom_gdpr_sdk_init);
         custom_gdpr_sdk_init.setOnClickListener(this);
 
-
+        native_ad_cache = findViewById(R.id.native_ad_cache);
+        native_ad_cache.setOnClickListener(this);
+        splash_ads = findViewById(R.id.splash_ads);
+        splash_ads.setOnClickListener(this);
         draw_video_view = (Button)findViewById(R.id.draw_video_view);
         draw_video_view.setOnClickListener(this);
         nativelist_ad = (Button)findViewById(R.id.nativelist_ad);
@@ -78,43 +85,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         native_advanced_btn.setOnClickListener(this);
         rewarded_video_btn = (Button)findViewById(R.id.rewarded_video_ad);
         rewarded_video_btn.setOnClickListener(this);
+        offerwall_ad = (Button)findViewById(R.id.offerwall_ad);
+        offerwall_ad.setOnClickListener(this);
         interstitial_ad = (Button)findViewById(R.id.interstitial_ad);
         interstitial_ad.setOnClickListener(this);
         banner_btn = (Button)findViewById(R.id.banner_ad);
         banner_btn.setOnClickListener(this);
         nativebanner_ad = (Button)findViewById(R.id.nativebanner_ad);
         nativebanner_ad.setOnClickListener(this);
-        splash_ads = findViewById(R.id.splash_ads);
-        splash_ads.setOnClickListener(this);
-        offerwall_ad = (Button)findViewById(R.id.offerwall_ad);
-        offerwall_ad.setOnClickListener(this);
 
         //欧盟GDPR等协议，发海外需要添加
         gpdrChild.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                TradPlusSdk.setGDPRChild(MainActivity.this,isChecked);
+                TradPlus.setGDPRChild(MainActivity.this,isChecked);
             }
         });
 
         CheckBox isChild = (CheckBox) findViewById(R.id.is_child);
-        Log.i("privacy", "onCreate coppa: "+TradPlusSdk.isCOPPAChild(MainActivity.this)+":ccpa:"+(TradPlusSdk.getCCPADataCollection(this) == TradPlusSdk.PRIVACY_ACCEPT_KEY));
-        isChild.setChecked(TradPlusSdk.isCOPPAChild(MainActivity.this) == TradPlusSdk.PRIVACY_ACCEPT_KEY);
+        Log.i("privacy", "onCreate coppa: "+TradPlus.IsCOPPAChild(MainActivity.this)+":ccpa:"+(TradPlus.getCCPADataCollection(this) == TradPlus.PRIVACY_ACCEPT_KEY));
+        isChild.setChecked(TradPlus.IsCOPPAChild(MainActivity.this) == TradPlus.PRIVACY_ACCEPT_KEY);
         isChild.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 Log.i("isChild", "onCheckedChanged: " + b);
-                TradPlusSdk.setCOPPAChild(MainActivity.this, b);
+                TradPlus.setCOPPAChild(MainActivity.this, b);
             }
         });
 
         cbCCPA = (CheckBox) findViewById(R.id.do_not_sell);
-        cbCCPA.setChecked(TradPlusSdk.getCCPADataCollection(MainActivity.this) == TradPlusSdk.PRIVACY_ACCEPT_KEY);
+        cbCCPA.setChecked(TradPlus.getCCPADataCollection(MainActivity.this) == TradPlus.PRIVACY_ACCEPT_KEY);
         cbCCPA.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 Log.i("cbCCPA", "onCheckedChanged: " + b);
-                TradPlusSdk.setCCPADataCollection(MainActivity.this, b);
+                TradPlus.setCCPADataCollection(MainActivity.this, b);
             }
         });
     }
@@ -127,12 +132,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 showGDPR();
                 break;
             case R.id.btn_gdpr_sdk_init:
-                if (!TradPlusSdk.getIsInit()) {
+                if (!TradPlus.isInit) {
                     initSdk();
                 }
                 break;
             case R.id.btn_yeu:
-                TradPlusSdk.setEUTraffic(MainActivity.this,true);
+                TradPlus.setEUTraffic(MainActivity.this,true);
                 if(isAccept){
                     hasShowGDPR();
                 }else {
@@ -146,7 +151,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 break;
             case R.id.btn_neu:
-                TradPlusSdk.setEUTraffic(MainActivity.this,false);
+                TradPlus.setEUTraffic(MainActivity.this,false);
                 if (unknownCountry != null) {
                     unknownCountry.setVisibility(View.GONE);
                 }
@@ -155,27 +160,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 break;
             case R.id.btn_gdpr_neu:
-                TradPlusSdk.setGDPRUploadDataLevel(MainActivity.this,TradPlusSdk.NONPERSONALIZED);
+                TradPlus.setGDPRUploadDataLevel(MainActivity.this,TradPlus.NONPERSONALIZED);
                 if (gdpr_container != null) {
                     gdpr_container.setVisibility(View.GONE);
                 }
                 break;
             case R.id.btn_gdpr_yeu:
-                TradPlusSdk.setGDPRUploadDataLevel(MainActivity.this,TradPlusSdk.PERSONALIZED);
+                TradPlus.setGDPRUploadDataLevel(MainActivity.this,TradPlus.PERSONALIZED);
                 if (gdpr_container != null) {
                     gdpr_container.setVisibility(View.GONE);
                 }
                 break;
             case R.id.btn_custom_gdpr_sdk_init:
-                if (!TradPlusSdk.getIsInit()) {
+                if (!TradPlus.isInit) {
                     initSdk1();
                 }
                 break;
+            case R.id.native_ad_cache:
+                startActivity(new Intent(MainActivity.this, NativeCacheActivity.class));
+                break;
             case R.id.splash_ads:
                 startActivity(new Intent(MainActivity.this, SplashActivity.class));
-                break;
-            case R.id.offerwall_ad:
-                startActivity(new Intent(MainActivity.this, OfferWallActivity.class));
                 break;
             case R.id.draw_video_view:
                 startActivity(new Intent(MainActivity.this, DrawNativeExpressVideoActivity.class));
@@ -188,10 +193,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.rewarded_video_ad:
                 Intent intent = new Intent(MainActivity.this, RewardedVideoActivity.class);
+                intent.putExtra("autoload",btn_box.isChecked());
                 startActivity(intent);
+                break;
+            case R.id.offerwall_ad:
+                startActivity(new Intent(MainActivity.this, OfferWallActivity.class));
                 break;
             case R.id.interstitial_ad:
                 Intent interstitialIntent = new Intent(MainActivity.this, InterstitialActivity.class);
+                interstitialIntent.putExtra("autoload",btn_box.isChecked());
                 startActivity(interstitialIntent);
                 break;
             case R.id.banner_ad:
@@ -204,7 +214,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void hasShowGDPR() {
-        if (!TradPlusSdk.isFirstShowGDPR(MainActivity.this)) {
+        if (!TradPlus.isFirstShowGDPR(MainActivity.this)) {
             showGDPR();
         } else {
             Toast.makeText(MainActivity.this, "您已经选择过，想修改请点击修改按钮！", Toast.LENGTH_LONG).show();
@@ -212,22 +222,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void showGDPR() {
-        TradPlusSdk.showUploadDataNotifyDialog(this, new TradPlusSdk.TPGDPRAuthListener() {
+        TradPlus.showUploadDataNotifyDialog(this, new ATGDPRAuthCallback() {
             @Override
             public void onAuthResult(int level) {
                 Log.i("level", "onAuthResult: "+level);
-                TradPlusSdk.setIsFirstShowGDPR(MainActivity.this,true);
+                TradPlus.setIsFirstShowGDPR(MainActivity.this,true);
             }
         }, Const.URL.GDPR_URL);
     }
 
     private void initSdk() {
         isAccept = true;
-        TradPlusSdk.setGDPRListener(new TradPlusSdk.TPGDPRListener() {
+        TradPlus.invoker().setmGDPRListener(new TradPlus.IGDPRListener() {
             @Override
             public void success(String msg) {
 
-                if (TradPlusSdk.isEUTraffic(MainActivity.this)) {
+                if (TradPlus.isEUTraffic(MainActivity.this)) {
                     if (gdprStatus != null) {
                         gdprStatus.setText("欧盟用户！");
                     }
@@ -246,7 +256,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             @Override
             public void failed(String errorMsg) {
-                Log.i("TradPlusSdk", "failed: "+errorMsg);
+                Log.i("tradplus", "failed: "+errorMsg);
                 //未知是否为欧盟
                 if (gdprStatus != null) {
                     gdprStatus.setText("未知国家");
@@ -257,8 +267,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
         //初始化SDK
-        TradPlusSdk.initSdk(this, TestAdUnitId.APPID);
-//        TradPlusSdk.setIsCNLanguageLog(true);//Log中文模式
+        TradPlus.invoker().initSDK(this, TestAdUnitId.APPID);
+//        TradPlus.setIsCNLanguageLog(true);//Log中文模式
         //设置测试模式，正式上线前注释
         TestDeviceUtil.getInstance().setNeedTestDevice(true);
 
@@ -266,11 +276,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void initSdk1() {
         isAccept = false;
-        TradPlusSdk.setGDPRListener(new TradPlusSdk.TPGDPRListener() {
+        TradPlus.invoker().setmGDPRListener(new TradPlus.IGDPRListener() {
             @Override
             public void success(String msg) {
 
-                if (TradPlusSdk.isEUTraffic(MainActivity.this)) {
+                if (TradPlus.isEUTraffic(MainActivity.this)) {
                     if (gdprCutomStatus != null) {
                         gdprCutomStatus.setText("欧盟用户！");
                     }
@@ -287,7 +297,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             @Override
             public void failed(String errorMsg) {
-                Log.i("TradPlusSdk", "failed: "+errorMsg);
+                Log.i("tradplus", "failed: "+errorMsg);
                 //未知是否为欧盟
                 if (gdprCutomStatus != null) {
                     gdprCutomStatus.setText("未知国家");
@@ -298,11 +308,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
-        TradPlusSdk.setPrivacyListener(new TradPlusSdk.TPPrivacyListener() {
+        TradPlus.invoker().setPrivacyListener(new TradPlus.IPrivacyListener() {
             @Override
             public void success(String msg) {
                 Log.i("ccpa", "success: ");
-                if (TradPlusSdk.isCalifornia(MainActivity.this)) {
+                if (TradPlus.isCalifornia(MainActivity.this)) {
                     cbCCPA.setVisibility(View.VISIBLE);
                 }else {
                     if (cbCCPA.getVisibility()== View.VISIBLE) {
@@ -317,8 +327,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
         //初始化SDK
-        TradPlusSdk.initSdk(this, TestAdUnitId.APPID);
-//        TradPlusSdk.setIsCNLanguageLog(true);//Log中文模式
+        TradPlus.invoker().initSDK(this, TestAdUnitId.APPID);
+//        TradPlus.setIsCNLanguageLog(true);//Log中文模式
 
         //设置测试模式，正式上线前注释
         TestDeviceUtil.getInstance().setNeedTestDevice(true);
